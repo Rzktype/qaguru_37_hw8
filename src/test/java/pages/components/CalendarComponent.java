a089c26dcc7f38950eaf686310d04440ebd6a105
Maxim <Rezusjkee@gmail.com>
2025-08-27 00:55:49 +0500

    Formated line in CalendarComponent

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -5,7 +5,6 @@ import static com.codeborne.selenide.Selenide.$;
 public class CalendarComponent {
     public void setDate(String day, String month, String year) {
         String formattedDay = String.format("%02d", Integer.parseInt(day));
-
         $(".react-datepicker__month-select").selectOption(month);
         $(".react-datepicker__year-select").selectOption(year);
         $(".react-datepicker__day--0" + formattedDay + ":not(.react-datepicker__day--outside-month)").click();
